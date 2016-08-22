53045fc96cfae4981da7c390a89eb815521d8b10
Thien Phu <lethienphu.1404@gmail.com>
2016-08-22 14:38:22 +0700

    lession scope and rootscope

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -3,12 +3,22 @@
 //Khởi tạo module
 var myApp = angular.module("myModule", []);
 
-//declare
-function myController($scope){
-    $scope.message = "This is controller of AngularJS";
-}
-
 //Khởi tạo controller
-myApp.controller("myController", myController);
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
 
-myController.$inject = ['$scope'];
+//declare
+function schoolController($scope) {
+    $scope.message = "Annountment from school";
+}
+function studentController($scope){
+    $scope.message = "This is message from student";
+}
+function teacherController($scope) {
+    $scope.message = "This is message from teacher";
+}
+//myController.$inject = ['$scope'];
+
+
+
