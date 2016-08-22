2f8084e60418f7fc8be6effab6666a4f17904e60
Thien Phu <lethienphu.1404@gmail.com>
2016-08-22 14:17:41 +0700

    bai17

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+//Khởi tạo module
+var myApp = angular.module("myModule", []);
+
+//declare
+function myController($scope){
+    $scope.message = "This is controller of AngularJS";
+}
+
+//Khởi tạo controller
+myApp.controller("myController", myController);
+
+myController.$inject = ['$scope'];
