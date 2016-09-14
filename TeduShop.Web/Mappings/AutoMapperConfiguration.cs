b93c0585274e9a1fdbba379d21788d4029694de2
Thien Phu <lethienphu.1404@gmail.com>
2016-09-14 12:14:22 +0700

    bai41

--- TeduShop.Web/Mappings/AutoMapperConfiguration.cs
+++ TeduShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -14,6 +14,7 @@ namespace TeduShop.Web.Mappings
             Mapper.CreateMap<ProductCategory, ProductCategoryViewModel>();
             Mapper.CreateMap<Product, ProductViewModel>();
             Mapper.CreateMap<ProductTag, ProductTagViewModel>();
+            Mapper.CreateMap<Footer, FooterViewModel>();
         }
     }
 }
