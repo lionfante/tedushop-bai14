24bf88b9eeabc8ed2801d24a4377bcf24a56d797
Thien Phu <lethienphu.1404@gmail.com>
2016-09-16 16:25:57 +0700

    bai46 - search and autocomplete

---
+++ TeduShop.Web/Assets/client/js/common.js
@@ -0,0 +1,38 @@
+﻿var common = {
+    init: function () {
+        common.registerEvents();
+    },
+    registerEvents: function () {
+        $("#txtKeyword").autocomplete({
+            minLength: 0,
+            source: function (request, response) {
+                $.ajax({
+                    url: "/Product/GetListProductByName",
+                    dataType: "json",
+                    data: {
+                        keyword: request.term
+                    },
+                    success: function (data) {
+                        response(data.data);
+                    }
+                });
+            },
+            focus: function (event, ui) {
+                $("#txtKeyword").val(ui.item.label);
+                return false;
+            },
+            select: function (event, ui) {
+                $("#txtKeyword").val(ui.item.label);
+
+                return false;
+            }
+        })
+        .autocomplete("instance")._renderItem = function (ul, item) {
+            return $("<li>")
+              .append("<div>" + item.label + "</div>")
+              .appendTo(ul);
+        };
+    }
+}
+
+common.init();
